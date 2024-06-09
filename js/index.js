989d7f212f5190fb522801910ef52536adfebebc
Torabarnob203311003 <118876454+maxarnob@users.noreply.github.com>
2024-06-09 23:19:12 +0600

    meals container dynamiclly added

--- js/index.js
+++ js/index.js
@@ -1,7 +1,37 @@
 const loadMeals = () => {
 fetch('https://www.themealdb.com/api/json/v1/1/search.php?f=a')
     .then(res => res.json())
-    .then ( data => console.log(data.meals))
+    .then ( data => displayMeals(data.meals))
 }
 
 loadMeals();
+
+ const displayMeals =(meals)=>{
+
+     const mealContainer = document.getElementById('meal-container');
+     meals.forEach(meal => {
+         console.log(meal)
+         const mealdiv = document.createElement('div');
+            mealdiv.classList.add('col');
+            mealdiv.innerHTML=`
+            
+                      <div class="card">
+                        <img src="..." class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional
+                                content. This content is a little bit longer.</p>
+                        </div>
+                    </div>
+            
+            
+            
+            
+            `;
+            mealContainer.appendChild(mealdiv);
+         
+
+
+
+     });
+ }
