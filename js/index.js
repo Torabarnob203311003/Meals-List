42a1ab656ab9ee1685dfc577acc4cdf6a16c3f41
Torabarnob203311003 <118876454+maxarnob@users.noreply.github.com>
2024-06-11 01:11:27 +0600

    displayed  meals details

--- js/index.js
+++ js/index.js
@@ -16,11 +16,10 @@ loadMeals();
             mealdiv.innerHTML=`
             
                       <div class="card">
-                        <img src="..." class="card-img-top" alt="...">
+                        <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
                         <div class="card-body">
                             <h5 class="card-title">${meal.strMeal}</h5>
-                            <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional
-                                content. This content is a little bit longer.</p>
+                            <p class="card-text">${meal.strInstructions.slice(0,220)}</p>
                         </div>
                     </div>
             
